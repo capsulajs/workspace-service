4d3797d679c67bddba6982115406228493d7ffff
MikeSquall <michael.arbib@om2.com>
2019-04-04 11:16:17 +0300

    Setup config for services in index

--- src/index.ts
+++ src/index.ts
@@ -3,45 +3,67 @@ import { Workspace } from './Workspace';
 const config = {
   name: 'POC',
   services: [
-    // {
-    //   serviceName: 'ParrotService',
-    //   displayName: 'Parrot',
-    //   path: './services/custom/ParrotService',
-    //   getInstance: (path: string, token: string) => {
-    //     return import(path).then((module: any) => {
-    //       console.log(path, module);
-    //       return new module.ParrotService(token);
-    //     });
-    //   },
-    //   options: {
-    //     definition: {
-    //       serviceName: 'ParrotService',
-    //       methods: {
-    //         repeat: { asyncModel: 'RequestResponse' },
-    //       },
-    //     },
-    //   },
-    // },
-    // {
-    //   serviceName: 'GreetingService',
-    //   displayName: 'Greeting',
-    //   path: './services/custom/GreetingService',
-    //   getInstance: (path: string, token: string) => {
-    //     return import(path).then((module: any) => {
-    //       console.log(path, module);
-    //       return new module.GreetingService(token);
-    //     });
-    //   },
-    //   options: {
-    //     definition: {
-    //       serviceName: 'GreetingService',
-    //       methods: {
-    //         hello: { asyncModel: 'RequestResponse' },
-    //         helloToParrot: { asyncModel: 'RequestResponse' },
-    //       },
-    //     }
-    //   },
-    // },
+    {
+      serviceName: 'EnvSelectorService',
+      displayName: 'EnvSelector',
+      path: '../src/services/custom/Selector',
+      getInstance: (path: string) => {
+        return import(path).then((module: any) => {
+          return new module.Selector();
+        });
+      },
+      options: {
+        definition: {
+          serviceName: 'EnvSelectorService',
+          methods: {
+            input: { asyncModel: 'RequestResponse' },
+            output$: { asyncModel: 'RequestStream' },
+            select: { asyncModel: 'RequestResponse' },
+            selected$: { asyncModel: 'RequestStream' }
+          },
+        },
+      },
+    },
+    {
+      serviceName: 'MethodSelectorService',
+      displayName: 'MethodSelector',
+      path: '../src/services/custom/Selector',
+      getInstance: (path: string) => {
+        return import(path).then((module: any) => {
+          return new module.Selector();
+        });
+      },
+      options: {
+        definition: {
+          serviceName: 'MethodSelectorService',
+          methods: {
+            input: { asyncModel: 'RequestResponse' },
+            output$: { asyncModel: 'RequestStream' },
+            select: { asyncModel: 'RequestResponse' },
+            selected$: { asyncModel: 'RequestStream' }
+          },
+        },
+      },
+    },
+    {
+      serviceName: 'EnvRegistryService',
+      displayName: 'EnvRegistry',
+      path: '../src/_custom_node_modules_/environment-registry/lib',
+      getInstance: (path: string, token: string) => {
+        return import(path).then((module: any) => {
+          return new module.EnvRegistry(token);
+        });
+      },
+      options: {
+        definition: {
+          serviceName: 'EnvRegistryService',
+          methods: {
+            register: { asyncModel: 'RequestResponse' },
+            environments$: { asyncModel: 'RequestStream' },
+          },
+        }
+      },
+    },
   ],
   components: [
     // {
@@ -64,7 +86,7 @@ const config = {
   ]
 };
 
-(window as any)['workspace'] = new Workspace({ token: 'abc', config});
-const workspace = (window as any)['workspace'];
+(window as any).workspace = new Workspace({ token: 'abc', config});
+const workspace = (window as any).workspace;
 
 workspace.start();
